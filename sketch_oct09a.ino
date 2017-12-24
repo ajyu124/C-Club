0fbea14739b4dbb7dfd5d62811210b3634e581ae
Angus Jyu <angusjyu@yahoo.com>
2017-12-24 00:22:04 -0800

    Add files via upload

---
+++ sketch_oct09a.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  digtialWrite(3, HIGH);
+  analogWrite(4, 30);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//analogWrite(PIN, VALUE);
+//digitalWrite(PIN, HIGH);
