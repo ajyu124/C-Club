02ea3132aaa2970280cd5201682e6de557822b73
Angus Jyu <angusjyu@yahoo.com>
2017-12-24 00:22:42 -0800

    Add files via upload

---
+++ sketch_oct23a.ino
@@ -0,0 +1,51 @@
+/**
+* Monday, 10/23/17
+* 
+* Meeting Agenda:
+* 1. Handout Arduinos + Materials
+* 2. RGB LED + Wiring Overview
+* 3. Quick Code Along
+* 4. Play with Colors
+* 
+* Handout Arduinos + Materials:
+* 1. Make sure you have filled out the mandatory form: https://goo.gl/forms/WSbxL6gOgqn398UM2
+* 2. Make sure you have filled out the team registration form: https://goo.gl/forms/ciacxVIuKNvZzJbx2
+*   a. If you have done this step already, please come up and let us know what your team name is
+* 3. Come up and receive the supplies and a handout
+* 4. When touching sensitive electronics, only grip the sides to prevent creating circuits with your hand oils
+* 5. Make sure you return the supplies at the end
+* 
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+/**
+* Wiring The Breadboard
+* 
+* 1. Breadboard is divided in half
+*   a. Each half is not connected in circuit to the other
+*   b. 1-a and 1-f are not connected
+* 2. The arrays of pins of "a, b, c, d, e" by "1, 2, 3..."
+*   a. Rows are separate from each other
+*   b. 1-a, 1-b, 1-c... are connected
+*   c. 1-a, 2-a, 2-a... are separated
+* 3. + and - sections are used to power each half of the breadboard
+*   a. They are called "power rails" and are powered with an external battery
+*   b. Not necessary for us
+*/
+
+/**
+* Play with Colors
+* https://www.w3schools.com/colors/colors_picker.asp
+* Feel free to choose whatever colors you want
+* Make sure it’s “SetColor(r, g, b)”
+* Play around with shapeshifting colors too! If you know how to do for-loops / while-loops, you can make gradients of colors
+
+*/
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
